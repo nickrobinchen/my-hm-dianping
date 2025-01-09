33086d04a689ec8cdb86c503e67a94ff55b84466
nick@macbook <931239318@qq.com>
2025-01-10 00:56:12 +0800

    Finished voucher 1

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,14 +1,26 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
+import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +29,75 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1. query voucher
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2. judge if second kill begin
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // not begin yet
+            return Result.fail("秒杀尚未开始");
+        }
+        // 3. judge if ended
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            // not begin yet
+            return Result.fail("秒杀已经结束");
+        }
+        // 4. judge if stock is plenty
+        if (voucher.getStock() < 1) {
+            return Result.fail("库存不足");
+        }
+
+        Long userId = UserHolder.getUser().getId();
+        // 创建锁对象
+        SimpleRedisLock lock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
+        boolean isLock = lock.tryLock(1200);
+        // judge if get lock success
+        if (!isLock) {
+            // failed
+            return Result.fail("一个人只允许下一单");
+        }
+        try {
+            return createVoucherOrder(voucherId);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Result createVoucherOrder(Long voucherId) {
+        Long userId = UserHolder.getUser().getId();
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+
+        if (count > 0) {
+            // 用户已经购买过了
+            return Result.fail("用户已经购买过了");
+        }
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)
+                .update();
+
+        if (!success) {
+            return Result.fail("库存不足");
+        }
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        voucherOrder.setUserId(userId);
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        return Result.ok(orderId);
+
+
+    }
 }
